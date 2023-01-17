0762e3db6e9022bbe610c25fb190188d9cc26f8f
Theo P <theo.park.n@gmail.com>
2023-01-16 20:05:05 -0500

    (script) installer script base

--- install.sh
+++ install.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
+# $1 = File to create a symlink for, $2 = target directory, $3 backup directory
+safe_symlink() {
+  if [[ -e "$2" ]]; then
+    mkdir -p "$3/"
+    mv "$2" "$3"
+    echo "$2" already exists. Moving to "$3"
+  fi
+  if [[ ! -e "$1" ]]; then
+    echo "$1" not found!
+  else
+    ln -sf "$1" "$2"
+    echo symlink for "$1" created at "$2"
+  fi
+}
 
+current_files=("init.lua" "lua")
+mkdir -p ~/.config/nvim/
+for v in ${current_files[@]}; do
+safe_symlink ~/.theovim/src/"$v" ~/.config/nvim/"$v" ~/theovim-install-backup
+done
 
