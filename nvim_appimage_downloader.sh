8eeaed2e327587255d21b0ff7a087aaed48fa40e
Theo P <theo.park.n@gmail.com>
2023-01-16 21:27:56 -0500

    (script) downloader error fix

--- nvim_appimage_downloader.sh
+++ nvim_appimage_downloader.sh
@@ -3,10 +3,18 @@
 mkdir -p ~/Applications/
 cd ~/Applications/ && { curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage ; chmod u+x nvim.appimage ; cd -; }
 
-echo "theovim) Would you like to add nvim alias to your bashrc (Please don't do it if you have done it already)? -> Type yes/y/Y."
+echo "theovim) Would you like to add nvim alias to your bashrc (Please don't do it if you have done it already)? -> Type y/Y."
 read -n1 alias_input
+echo
 case $alias_input in
-  yes|y|Y)
-    echo "alias nvim ~/Applications//nvim.appimage" >> ~/.bashrc
+  y|Y)
+    echo -e "\n#Alias for those who prefer nvim appimage - Theovim" >> ~/.bashrc
+    echo -e "alias nvim=~/Applications/nvim.appimage\n" >> ~/.bashrc
+    source ~/.bashrc
+    echo "Bashrc has been modified and reloaded"
+  ;;
+  *)
+    echo "Bye!"
+  ;;
 esac
 
