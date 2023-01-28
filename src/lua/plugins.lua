a438ae8cd976b48ba860b34282fe83c656796964
Theo P <theo.park.n@gmail.com>
2023-01-28 16:35:15 -0500

    (plugin-manager) from packer to lazy - done

--- src/lua/plugins.lua
+++ src/lua/plugins.lua
@@ -7,88 +7,83 @@
 "                                 |_|       |___|
 --]]
 
---- {{{ Packer installation
-local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-end
-
-local packer_group = vim.api.nvim_create_augroup("Packer", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePost", { command = "source <afile> | PackerCompile",
-  group = packer_group, pattern = "init.lua" })
--- }}}
-
--- {{{ Packer Plugins
-require("packer").startup(function(use)
+-- {{{
+local plugins = {
   -- {{{ Dependencies
-  use "wbthomason/packer.nvim" --> Irony of having to import itself
-  use "nvim-lua/plenary.nvim" --> Lua function library for Neovim
-  use "kyazdani42/nvim-web-devicons" --> Icons for barbar, Telescope, and more
+  { "nvim-lua/plenary.nvim" }, --> Lua function library for Neovim
+  { "kyazdani42/nvim-web-devicons" }, --> Icons for barbar, Telescope, and more
   -- }}}
 
   -- {{{ Appearance
-  use "navarasu/onedark.nvim" --> Pretty theme
-  use "nvim-lualine/lualine.nvim" --> Status line plugin
-  use "romgrk/barbar.nvim" --> Tab bar plugin
-  use "glepnir/dashboard-nvim" --> Startup dashboard
-  use {
-    "folke/zen-mode.nvim", --> Centered view for focused text editing
+  { "navarasu/onedark.nvim" }, --> Pretty theme
+  { "nvim-lualine/lualine.nvim" }, --> Status line plugin
+  { "romgrk/barbar.nvim" }, --> Tab bar plugin
+  { "glepnir/dashboard-nvim" }, --> Startup dashboard
+  { "folke/zen-mode.nvim", --> Centered view for foc{d text editing
     config = function() require("zen-mode").setup() end
-  }
-  use "rcarriga/nvim-notify" --> Prettier notification
+  },
+  { "rcarriga/nvim-notify" }, --> Prettier notification
   -- }}}
 
   -- {{{ File et Search
-  use {
-    "lewis6991/gitsigns.nvim", --> Git information
+  { "lewis6991/gitsigns.nvim", --> Git information
     config = function() require("gitsigns").setup() end
-  }
-  use {
-    "lukas-reineke/indent-blankline.nvim", --> Indentation guide
+  },
+  { "lukas-reineke/indent-blankline.nvim", --> Indentation guide
     config = function() require("indent_blankline").setup() end
-  }
-  use "nvim-treesitter/nvim-treesitter" --> Incremental highlighting
-  use "p00f/nvim-ts-rainbow" --> Rainbow color matching for parentheses
-  use "kyazdani42/nvim-tree.lua" --> File tree
-  use "nvim-telescope/telescope.nvim" --> Expendable fuzzy finder
-  use "nvim-telescope/telescope-file-browser.nvim" --> File browser extension for Telescope
-  use "folke/which-key.nvim" --> Pop-up dictionary for keybindings
-  use {
-    "ellisonleao/glow.nvim", --> Markdown file preview. Requires glow installed
+  },
+  { "nvim-treesitter/nvim-treesitter" }, --> Incremental highlighting
+  { "p00f/nvim-ts-rainbow" }, --> Rainbow color matching for parentheses
+  { "kyazdani42/nvim-tree.lua" }, --> File tree
+  { "nvim-telescope/telescope.nvim" }, --> Expendable fuzzy finder
+  { "nvim-telescope/telescope-file-browser.nvim" }, --> File browser extension for Telescope
+  { "folke/which-key.nvim" }, --> Pop-up dictionary for keybindings
+  { "ellisonleao/glow.nvim", --> Markdown file preview. Requires glow installed
     ft = { "markdown" },
-  }
-  use({
-    "iamcco/markdown-preview.nvim", --> MarkdownPreview to toggle
+  },
+  { "iamcco/markdown-preview.nvim", --> MarkdownPreview to toggle
     run = function() vim.fn["mkdp#util#install"]() end, --> Binary installation for markdown-preview
-  })
+  },
   -- }}}
 
   -- {{{ LSP
-  use "neovim/nvim-lspconfig" --> Neovim defult LSP engine
-  use {
-    "williamboman/mason.nvim", --> LSP Manager
+  { "neovim/nvim-lspconfig" }, --> Neovim defult LSP engine
+  { "williamboman/mason.nvim", --> LSP Manager
     config = function() require("mason").setup() end
-  }
-  use "theopn/friendly-snippets" --> VS Code style snippet collection
-  use {
-    "L3MON4D3/LuaSnip", --> Snippet engine that accepts VS Code style snippets
+  },
+  { "theopn/friendly-snippets" }, --> VS Code style snippet collection
+  { "L3MON4D3/LuaSnip", --> Snippet engine that accepts VS Code style snippets
     config = function() require("luasnip.loaders.from_vscode").lazy_load() end --> Load snippets from friendly snippets
-  }
-  use "saadparwaiz1/cmp_luasnip" --> nvim_cmp and LuaSnip bridge
-  use "hrsh7th/cmp-nvim-lsp" --> nvim-cmp and lspconfig bridge
-  use "hrsh7th/cmp-buffer" --> nvim-cmp source for buffer words
-  use "hrsh7th/cmp-path" --> nvim-cmp source for file path
-  use "hrsh7th/cmp-cmdline" --> nvim-cmp source for vim commands
-  use "hrsh7th/nvim-cmp" --> Completion Engine
-  use "folke/trouble.nvim" --> Pretty list of LSP error list
-  use {
-    "glepnir/lspsaga.nvim", --> LSP hover menu, code action, rename, etc
+  },
+  { "saadparwaiz1/cmp_luasnip" }, --> nvim_cmp and LuaSnip bridge
+  { "hrsh7th/cmp-nvim-lsp" }, --> nvim-cmp and lspconfig bridge
+  { "hrsh7th/cmp-buffer" }, --> nvim-cmp source for buffer words
+  { "hrsh7th/cmp-path" }, --> nvim-cmp source for file path
+  { "hrsh7th/cmp-cmdline" }, --> nvim-cmp source for vim commands
+  { "hrsh7th/nvim-cmp" }, --> Completion Engine
+  { "folke/trouble.nvim" }, --> Pretty list of LSP error list
+  { "glepnir/lspsaga.nvim", --> LSP hover menu, code action, rename, etc
     branch = "main",
-    config = function()
-      require('lspsaga').setup({})
-    end,
-  }
-  -- }}}
-
-end)
+    config = function() require('lspsaga').setup({}) end
+  },
+}
 -- }}}
+
+
+--- {{{ Lazy.nvim installation
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(plugins)
+--- }}}
+
