9d4498e139ab19243c20422166b6e64b2f512d87
Theo P <theo.park.n@gmail.com>
2023-02-03 11:18:46 -0500

    (weather) optional location argument

--- src/lua/theovim.lua
+++ src/lua/theovim.lua
@@ -33,25 +33,23 @@ local function theovim_floating_win_util(file_path)
   local win = vim.api.nvim_open_win(buf, true, win_opts)
 
   -- options
-  vim.api.nvim_win_set_option(win, "winblend", 0) --> How much does the background color blends in (80 will be blac)
+  vim.api.nvim_win_set_option(win, "winblend", 0) --> How much does the background color blends in (80 will be black)
   vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe") --> Kill the buffer when hidden
   vim.api.nvim_buf_set_option(buf, "filetype", "theovimFloatingWin")
 
   -- keymaps
   local keymaps_opts = { silent = true, buffer = buf }
-  vim.keymap.set("n", "q", "<C-w>q", keymaps_opts)
-  vim.keymap.set("n", "<ESC>", function() vim.api.nvim_win_close(win, true) end, keymaps_opts)
+  vim.keymap.set('n', "q", "<C-w>q", keymaps_opts)
+  vim.keymap.set('n', "<ESC>", function() vim.api.nvim_win_close(win, true) end, keymaps_opts)
 
   -- https://www.reddit.com/r/neovim/comments/s97tja/opening_an_existing_file_in_a_floating_window/
   vim.api.nvim_buf_set_option(0, "modifiable", true)
   vim.api.nvim_command("$read" .. file_path)
   vim.api.nvim_buf_set_option(0, "modifiable", false)
-
 end
 
 vim.api.nvim_create_user_command("TheovimUpdate", function()
   vim.api.nvim_command(":! cd ~/.theovim && git pull && ./theovim-util.sh update")
-  local changelog_path = vim.api.nvim_get_runtime_file("changelog.txt", false)[1]
   vim.notify("Update complete. Use :TheovimInfo command to see the latest changelog")
   require('lazy').sync()
 end, { nargs = 0 })
@@ -61,12 +59,11 @@ local helpdoc_path = vim.api.nvim_get_runtime_file("theovim-help-doc.md", false)
 vim.api.nvim_create_user_command("TheovimHelp", function() theovim_floating_win_util(helpdoc_path) end, { nargs = 0 })
 
 local info_path = vim.api.nvim_get_runtime_file("theovim-info.txt", false)[1]
-vim.api.nvim_create_user_command("TheovimInfo", function() theovim_floating_win_util(info_path) end,
-  { nargs = 0 })
+vim.api.nvim_create_user_command("TheovimInfo", function() theovim_floating_win_util(info_path) end, { nargs = 0 })
 
 
 -- Simplified version of https://github.com/ellisonleao/weather.nvim
-local function weather_popup()
+local function weather_popup(location)
   -- window size and pos
   local win_height = math.ceil(vim.o.lines * 0.6 - 20)
   local win_width = math.ceil(vim.o.columns * 0.3 - 15)
@@ -89,12 +86,17 @@ local function weather_popup()
   vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
   vim.api.nvim_win_set_option(win, "winblend", 0)
 
-  vim.keymap.set('n', 'q', function() vim.api.nvim_win_close(win, true) end, { noremap = true, silent = true })
-  vim.keymap.set('n', '<ESC>', function() vim.api.nvim_win_close(win, true) end, { noremap = true, silent = true })
+  local keymaps_opts = { silent = true, buffer = buf }
+  vim.keymap.set('n', "q", "<C-w>q", keymaps_opts)
+  vim.keymap.set('n', "<ESC>", function() vim.api.nvim_win_close(win, true) end, keymaps_opts)
 
-  local command = "curl 'https://wttr.in/?0T'"
-  vim.fn.termopen(command)
+  local weather_command = "curl 'https://wttr.in/?0T'"
+  if location ~= nil then
+    weather_command = string.format("curl https://wttr.in/%s'?'0", location)
+  end
+  vim.fn.termopen(weather_command)
 end
 
-vim.api.nvim_create_user_command("Weather", function() weather_popup() end,
-  { nargs = 0 })
+-- In the above user command creation, I passed the return values of function() my_func() end as the command
+-- In here I am passing the function as whole, so it can take arguments from the user command
+vim.api.nvim_create_user_command("Weather", weather_popup, { nargs = '?' }) --> ?: 0 or 1, *: > 0, +: > 1
