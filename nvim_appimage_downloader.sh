2ab97226830455cbf9ee7fe984d52c8ff2fe8b4b
Theo P <theo.park.n@gmail.com>
2023-02-12 01:04:24 -0500

    (script) applimage downloader folder name changed

--- nvim_appimage_downloader.sh
+++ nvim_appimage_downloader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p ~/Applications/
-cd ~/Applications/ && { curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage ; chmod u+x nvim.appimage ; cd -; }
+mkdir -p ~/apps/
+cd ~/apps/ & { curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage ; chmod u+x nvim.appimage ; cd -; }
 
 echo "theovim) Would you like to add nvim alias to your bashrc (Please don't do it if you have done it already)? -> Type y/Y."
 read -n1 alias_input
@@ -9,7 +9,7 @@ echo
 case $alias_input in
   y|Y)
     echo -e "\n#Alias for those who prefer nvim appimage - Theovim" >> ~/.bashrc
-    echo -e "alias nvim=~/Applications/nvim.appimage\n" >> ~/.bashrc
+    echo -e "alias nvim=~/apps/nvim.appimage\n" >> ~/.bashrc
     source ~/.bashrc
     echo "Bashrc has been modified and reloaded"
   ;;
