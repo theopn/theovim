a378b8bb0b47eec3c7b00d72bbc33ee8dc89e89c
Theo P <theo.park.n@gmail.com>
2023-02-13 20:16:09 -0500

    (file editing) template auto group bug fixed

--- src/lua/file_et_search.lua
+++ src/lua/file_et_search.lua
@@ -8,61 +8,61 @@
 --]]
 -- {{{ Tree Sitter Settings
 require("nvim-treesitter.configs").setup {
-    ensure_installed = { "c", "latex", "lua", "markdown", "python", "vim", },
-    sync_install = false,
-    auto_install = true,
-    ignore_install = {},
-    highlight = {
-        enable = true,
-        disable = {},
-        additional_vim_regex_highlighting = false,
-    },
-    rainbow = {
-        enable = true,
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    }
+  ensure_installed = { "c", "latex", "lua", "markdown", "python", "vim", },
+  sync_install = false,
+  auto_install = true,
+  ignore_install = {},
+  highlight = {
+    enable = true,
+    disable = {},
+    additional_vim_regex_highlighting = false,
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+  }
 }
 -- }}}
 
 -- {{{ NvimTree Settings
 require("nvim-tree").setup {
-    auto_reload_on_write = true,
-    open_on_setup = false, --> Auto open when no files opened
-    open_on_setup_file = false, --> Auto open when files opened
-    open_on_tab = false,
-    sort_by = "name",
-    view = {
-        width = 30,
-        --height = 30, --> No longer supported
-        hide_root_folder = false,
-        side = "left",
-        preserve_window_proportions = false,
-        number = false,
-        relativenumber = false,
-        signcolumn = "yes",
-    }
+  auto_reload_on_write = true,
+  open_on_setup = false, --> Auto open when no files opened
+  open_on_setup_file = false, --> Auto open when files opened
+  open_on_tab = false,
+  sort_by = "name",
+  view = {
+    width = 30,
+    --height = 30, --> No longer supported
+    hide_root_folder = false,
+    side = "left",
+    preserve_window_proportions = false,
+    number = false,
+    relativenumber = false,
+    signcolumn = "yes",
+  }
 }
 -- }}}
 
 -- {{{ Telescope Settings
 require("telescope").setup {
-    defaults = {
-        mappings = {
-            i = {
-                ["<C-j>"] = "move_selection_next",
-                ["<C-k>"] = "move_selection_previous",
-            },
-        },
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-j>"] = "move_selection_next",
+        ["<C-k>"] = "move_selection_previous",
+      },
     },
-    extensions = { file_browser = { hidden = true } },
+  },
+  extensions = { file_browser = { hidden = true } },
 }
 require("telescope").load_extension("file_browser")
 
 -- Comprehensive list menu for Telescope functionalities
 local telescope_options = {
-    ["Git Commits"] = function() vim.cmd("Telescope git_commits") end,
-    ["Git Status"] = function() vim.cmd("Telescope git_status") end
+  ["Git Commits"] = function() vim.cmd("Telescope git_commits") end,
+  ["Git Status"] = function() vim.cmd("Telescope git_status") end
 }
 local telescope_option_names = {}
 local n = 0
@@ -73,14 +73,14 @@ end
 table.sort(telescope_option_names)
 function THEOVIM_TELESCOPE_MENU()
   vim.ui.select(telescope_option_names, {
-      prompt = "Telescope option to open",
+    prompt = "Telescope option to open",
   },
-      function(choice)
-        local action_func = telescope_options[choice]
-        if action_func ~= nil then
-          action_func()
-        end
-      end)
+    function(choice)
+      local action_func = telescope_options[choice]
+      if action_func ~= nil then
+        action_func()
+      end
+    end)
 end
 
 -- }}}
@@ -91,19 +91,20 @@ vim.g.vimtex_view_method = "skim"
 --}}}
 
 -- {{{ Templates
+local template_augroup = vim.api.nvim_create_augroup("Template", { clear = true })
 vim.api.nvim_create_autocmd("BufNewFile", {
-    group = vim.api.nvim_create_augroup("Template", { clear = true }),
-    pattern = "*.tex",
-    callback = function()
-      vim.cmd("0r ~/.theovim/templates/latex-hw-template.tex")
-    end
+  group = template_augroup,
+  pattern = "*.tex",
+  callback = function()
+    vim.cmd("0r ~/.theovim/templates/latex-hw-template.tex")
+  end
 })
 vim.api.nvim_create_autocmd("BufNewFile", {
-    group = vim.api.nvim_create_augroup("Template", { clear = true }),
-    pattern = "*.h",
-    callback = function()
-      vim.cmd("0r ~/.theovim/templates/c-header-template.h")
-    end
+  group = template_augroup,
+  pattern = "*.h",
+  callback = function()
+    vim.cmd("0r ~/.theovim/templates/c-header-template.h")
+  end
 })
 --}}}
 
@@ -140,13 +141,13 @@ end
 
 function THEOVIM_REGISTER_MENU()
   vim.ui.select(get_register_table(), {
-      prompt = "Register content to paste at the current cursor:",
+    prompt = "Register content to paste at the current cursor:",
   },
-      function(choice)
-        if choice == nil then return end
-        local content = format_register_table(choice)
-        vim.cmd("put " .. content[1])
-      end)
+    function(choice)
+      if choice == nil then return end
+      local content = format_register_table(choice)
+      vim.cmd("put " .. content[1])
+    end)
 end
 
 -- }}}
