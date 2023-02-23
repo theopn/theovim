6abcc7b1aa22d796680b513581bea26851742bfe
Theo P <theo.park.n@gmail.com>
2023-02-23 15:05:11 -0500

    (config) template pdf viewer:

--- config_template.lua
+++ config_template.lua
@@ -1,5 +1,6 @@
 -- Include your *Lua* personal configuration below
 -- Find the example configuration at ~/.theovim/config_template.lua
 
--- Setting PDF viewer for a live compilation
+-- Setting PDF viewer for a live compilation. Skim for Mac and Zathura for Linux
 vim.g.vimtex_view_method = "skim"
+--vim.g.vimtex_view_method = "zathura"
