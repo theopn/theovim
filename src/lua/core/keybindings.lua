6f15f2132b1c11db1ef65dedff765d100f8ac4cb
Theo P <theo.park.n@gmail.com>
2023-02-26 19:48:11 -0500

    (look) barbar is now replaced by tabby

--- src/lua/core/keybindings.lua
+++ src/lua/core/keybindings.lua
@@ -31,6 +31,18 @@ local key_opt = {
   -- Normal Mode --
   { 'n', "<leader>/",       "<CMD>let @/=''<CR>" }, --> @/ is the macro for the last search
   { 'n', "<leader>a",       "gg<S-v>G" }, --> Select all
+  -- Search auto center --
+  { 'n', "n",               "nzz" },
+  { 'n', "N",               "Nzz" },
+  -- Copy and paste --
+  { 'v', "<leader>y",       '"+y' }, --> Copy to the system clipboard
+  { 'n', "<leader>p",       "<CMD>Telescope registers<CR>" }, --> ":reg" W/O PLUGIN
+  -- Terminal Mode --
+  { 't', "<ESC>",           "<C-\\><C-n>" }, --> ESC for term
+  -- Spell check --
+  { 'i', "<C-s>",           "<C-g>u<ESC>[s1z=`]a<C-g>u" }, --> Fix nearest spelling error and put the cursor back
+  { 'n', "<C-s>",           "z=" }, --> Toggle spelling suggestions
+  { 'n', "<leader>st",      "<CMD>set spell!<CR>" }, --> Toggle spellcheck
   -- Split pane --
   { 'n', "<leader>|",       "<CMD>vsplit<CR><C-w>l" },
   { 'n', "<leader>-",       "<CMD>split<CR><C-w>j" },
@@ -43,22 +55,22 @@ local key_opt = {
   { 'n', "<leader><DOWN>",  "<C-w>5-" },
   { 'n', "<leader><UP>",    "<C-w>5+" },
   { 'n', "<leader><RIGHT>", "<C-w>10>" },
-  -- Buffer naviagation --
-  { 'n', "<leader>n",       "<CMD>enew<CR>" }, --> Open a new buffer
-  { 'n', "<leader>,",       "<CMD>bprevious<CR>" },
-  { 'n', "<leader>.",       "<CMD>bnext<CR>" },
-  { 'n', "<leader>x",       "<CMD>bdelete<CR>" }, --> Kill a buffer
-  -- Search auto center --
-  { 'n', "n",               "nzz" },
-  { 'n', "N",               "Nzz" },
-  -- Visual Mode --
-  { 'v', "<leader>y",       '"+y' }, --> Copy to the system clipboard
-  -- Terminal Mode --
-  { 't', "<ESC>",           "<C-\\><C-n>" }, --> ESC for term
-  -- Spell check --
-  { 'i', "<C-s>",           "<C-g>u<ESC>[s1z=`]a<C-g>u" }, --> Fix nearest spelling error and put the cursor back
-  { 'n', "<C-s>",           "z=" }, --> Toggle spelling suggestions
-  { 'n', "<leader>st",      "<CMD>set spell!<CR>" }, --> Toggle spellcheck
+  -- Tab --
+  { 'n', "<leader>n",       ":ls<CR>:echo '* Enter: New tab w/ curr buf'<CR>:echo '* Buf # + Enter: New tab w/ selected buf'<CR>:tab sb<SPACE>" },
+  { 'n', "<leader>1",       "1gt" }, --> Go to 1st tab
+  { 'n', "<leader>2",       "2gt" }, --> ^
+  { 'n', "<leader>3",       "3gt" }, --> ^
+  { 'n', "<leader>4",       "4gt" }, --> ^
+  { 'n', "<leader>5",       "5gt" }, --> ^
+  { 'n', "<leader>6",       "6gt" }, --> ^
+  { 'n', "<leader>7",       "7gt" }, --> ^
+  { 'n', "<leader>8",       "8gt" }, --> ^
+  { 'n', "<leader>7",       "9gt" }, --> ^
+  -- Buffer --
+  { 'n', "<leader>b",       "<CMD>Telescope buffers<CR>" }, --> ":ls<CR>:b<SPACE>" W/O PLUGIN
+  { 'n', "<leader>,",       "<CMD>bprevious<CR>" }, --> Cycle through the buffer
+  { 'n', "<leader>.",       "<CMD>bnext<CR>" }, --> ^
+  { 'n', "<leader>x",       ":ls<CR>:echo '* Enter: Delete curr buf'<CR>:echo '* Buf # + Enter: Delete/ selected buf'<CR>:bdelete<SPACE>" },
   -- }}}
 
   -- {{{ Plugin/Feature Specific Keybindings
@@ -67,14 +79,13 @@ local key_opt = {
   { 'n', "<leader>?",       "<CMD>WhichKey<CR>" }, --> Bring up Which-key pop-up
   { 'n', "<leader>t",       "<CMD>NvimTreeToggle<CR>" }, --> Tree toggle
   -- Telescope --
-  { 'n', "<leader>p",       "<CMD>Telescope registers<CR>" },
   { 'n', "<leader>fa",      function() THEOVIM_TELESCOPE_MENU() end },
   { 'n', "<leader>ff",      "<CMD>Telescope find_files<CR>" },
   { 'n', "<leader>fb",      "<CMD>Telescope file_browser<CR>" },
   { 'n', "<leader>f/",      "<CMD>Telescope current_buffer_fuzzy_find<CR>" },
   -- LSP --
   { 'n', "<leader>ca",      function() THEOVIM_LSP_MENU() end },
-  { 'n', "<leader>cd",      function() vim.lsp.buf.hover() end },
+  { 'n', "<leader>cd",      function() vim.lsp.buf.hover() end }, --> LSPSage requires markdown treesitter
   { 'n', "<leader>cr",      function() require("lspsaga.rename"):lsp_rename() end }, --> vim.lsp.buf.rename()
   -- }}}
 }
