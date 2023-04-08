728a16a60ddff8934f78b1c9252993e53a6b8f39
Theo P <theo.park.n@gmail.com>
2023-04-08 14:54:03 -0400

    (look) dashboard revision done! (I think)

--- src/lua/look/dashboard.lua
+++ src/lua/look/dashboard.lua
@@ -53,12 +53,12 @@ local olivers = {
 local header = olivers[math.random(#olivers)]
 
 -- figlet -f small theovim
-local message = {
-  os.date("[ THEOVIM %m-%d %H:%m ]"),
-  "[ ━━━━━━━━ ❖ ━━━━━━━━ ]",
-  -- " ___                    ",
-  -- "  | |_  _  _     o __   ",
-  -- "  | | |(/_(_)\\_/ | |||  ",
+local logo = {
+  " ___                    ",
+  "  | |_  _  _     o __   ",
+  "  | | |(/_(_)\\_/ | |||  ",
+  "",
+  os.date("[ ━━%m-%d━━ ❖ ━━%H:%M━━ ]"),
 }
 
 local buttons = {
@@ -67,7 +67,7 @@ local buttons = {
   { "  File Browser  ",  "SPC f b", "Telescope file_browser" },
   { "  New File      ",  "       ", "enew" },
   { "  Config Theovim",  "       ", "e ~/.config/nvim/lua/user_config.lua" },
-  { "  Exit Theovim  ",  "ZZ     ", "quit" },
+  { "  Exit Theovim  ",  "     ZZ", "quit" },
 }
 -- }}}
 
@@ -77,13 +77,14 @@ local buttons = {
 local emptyLine = string.rep(" ", vim.fn.strwidth(header[1]))
 table.insert(header, 1, emptyLine)
 header[#header + 1] = emptyLine
-message[#message + 1] = emptyLine
-message[#message + 1] = emptyLine
+logo[#logo + 1] = emptyLine
 
 local width = #header[1] + 3
-local max_height = #header + 8 + (2 * #buttons) -- 8 == top bottom extra spaces + message
+local max_height = #header + 2 + #logo + 1 + (#buttons * 2) + 1 + 1 -- Numbers are paddings + one extra safety net
 
 -- Function to create a new dashboard on a startup
+-- Inspired from: https://github.com/chadcat7/kodo/blob/main/lua/ui/dash/init.lua
+--                https://github.com/NvChad/ui/blob/dev/lua/nvchad_ui/nvdash/init.lua
 local function open()
   -- Condition check and table init --
 
@@ -109,8 +110,8 @@ local function open()
     return string.rep(" ", math.floor(pad)) .. str .. " "
   end
 
-  -- add spacing to the buttons
-  local addSpacingToBtns = function(txt1, txt2)
+  -- Function to format the button icon, description, and shortcuts
+  local formatBtns = function(txt1, txt2)
     local btn_len = vim.fn.strwidth(txt1) + vim.fn.strwidth(txt2)
     local spacing = vim.fn.strwidth(header[1]) - btn_len
     return txt1 .. string.rep(" ", spacing - 1) .. txt2 .. " "
@@ -120,13 +121,14 @@ local function open()
   for _, val in ipairs(header) do
     table.insert(dashboard, val .. " ")
   end
-  for _, val in ipairs(message) do
+  for _, val in ipairs(logo) do
     table.insert(dashboard, val .. " ")
   end
   for _, val in ipairs(buttons) do
-    table.insert(dashboard, addSpacingToBtns(val[1], val[2]) .. " ")
-    table.insert(dashboard, header[1] .. " ")
+    table.insert(dashboard, formatBtns(val[1], val[2]) .. " ") -- Button formatting
+    table.insert(dashboard, emptyLine .. " ")                  -- New line
   end
+  table.remove(dashboard, #dashboard)                          -- Remove the extra new line padding from the buttons
 
   --------------------
 
@@ -137,20 +139,20 @@ local function open()
     result[i] = ""
   end
 
+  local headerStartButActlyForHeader = math.floor((vim.api.nvim_win_get_height(0) / 2) - (#dashboard / 2) - 1)
   local headerStart = math.floor((vim.api.nvim_win_get_height(0) / 2) - (#dashboard / 2) - 1)
-  local start = math.floor((vim.api.nvim_win_get_height(0) / 2) - (#dashboard / 2) - 1)
 
   -- adding the dashboard
   for _, val in ipairs(dashboard) do
-    result[headerStart] = addPaddingToHeader(val)
-    headerStart = headerStart + 1
+    result[headerStartButActlyForHeader] = addPaddingToHeader(val)
+    headerStartButActlyForHeader = headerStartButActlyForHeader + 1
   end
 
   -- setting the dasboard
   vim.api.nvim_buf_set_lines(buf, 0, -1, false, result)
 
   -- setting the cursor
-  vim.api.nvim_win_set_cursor(0, { start + #header + #message, math.floor(vim.o.columns / 2) - 18 })
+  vim.api.nvim_win_set_cursor(0, { headerStart + #header + #logo, math.floor(vim.o.columns / 2) - 15 })
 
   ---------------------------
 
@@ -159,65 +161,56 @@ local function open()
   local ThVimDashHl = vim.api.nvim_create_namespace("ThVimDashHl")
   local horiz_pad_index = math.floor((vim.api.nvim_win_get_width(0) / 2) - (width / 2)) - 2
 
-  for i = start, start + #header - 2 do
+  for i = headerStart, headerStart + #header - 2 do
     vim.api.nvim_buf_add_highlight(buf, ThVimDashHl, "ThVimLogoHl", i, horiz_pad_index, -1)
   end
-  for i = start + #header - 2, start + #header + #message - 2 do
+  for i = headerStart + #header - 2, headerStart + #header + #logo - 2 do
     vim.api.nvim_buf_add_highlight(buf, ThVimDashHl, "ThVimMsgHl", i, horiz_pad_index, -1)
   end
-  for i = start + #header + #message - 2, start + #header + #buttons * 2 + 1 do
+  for i = headerStart + #header + #logo - 2, headerStart + #header + (#buttons * 2) + 1 do
     vim.api.nvim_buf_add_highlight(buf, ThVimDashHl, "ThVimButtonsHl", i, horiz_pad_index, -1)
-  end
-  for i = start + #header + #message - 1, start + #header + #buttons * 2 + 1, 2 do
-    vim.api.nvim_buf_add_highlight(buf, ThVimDashHl, "ThVimButtonsHl", i, horiz_pad_index + 40,
-      horiz_pad_index + 42 + string.len(buttons[1][2]))
-    vim.api.nvim_buf_add_highlight(buf, ThVimDashHl, "DashButtonsSpl", i, horiz_pad_index + 3, horiz_pad_index + 7)
   end
 
   -----------------------
 
   -- Keybindings --
 
-  local firstbtnline = start + #header + #message + 2
+  local firstbtnline = headerStart + #header + #logo + 2
   local keybinds = {}
 
+  -- Bug: It has extra lines above and below the buttons each,
+  -- but table.remove or/and not subtracting 2 initially does not work
   for _, _ in ipairs(buttons) do
     table.insert(keybinds, firstbtnline - 2)
     firstbtnline = firstbtnline + 2
   end
+  --table.remove(keybinds, #keybinds)
 
+  local upMvmt = function()
+    local cur = vim.fn.line(".") -- Current line
+    -- Check if the current line number is button or move to the last element
+    local target_line = vim.tbl_contains(keybinds, cur) and cur - 2 or keybinds[#keybinds]
+    vim.api.nvim_win_set_cursor(0, { target_line, math.floor(vim.o.columns / 2) - 15 })
+  end
+  local downMvmt = function()
+    local cur = vim.fn.line(".") -- Current line
+    -- Check if the current line number is button or move to the first element
+    local target_line = vim.tbl_contains(keybinds, cur) and cur + 2 or keybinds[1]
+    vim.api.nvim_win_set_cursor(0, { target_line, math.floor(vim.o.columns / 2) - 15 })
+  end
   vim.keymap.set("n", "h", "", { buffer = true })
+  vim.keymap.set("n", "j", downMvmt, { buffer = true })
+  vim.keymap.set("n", "k", upMvmt, { buffer = true })
   vim.keymap.set("n", "l", "", { buffer = true })
+  vim.keymap.set("n", "<LEFT>", "", { buffer = true })
+  vim.keymap.set("n", "<DOWN>", downMvmt, { buffer = true })
+  vim.keymap.set("n", "<UP>", upMvmt, { buffer = true })
+  vim.keymap.set("n", "<RIGHT>", "", { buffer = true })
 
-  vim.keymap.set("n", "k", function()
-    local cur = vim.fn.line "."
-    local target_line = vim.tbl_contains(keybinds, cur) and cur - 2 or keybinds[#keybinds]
-    vim.api.nvim_win_set_cursor(0, { target_line, math.floor(vim.o.columns / 2) - 13 })
-  end, { buffer = true })
-
-  vim.keymap.set("n", "j", function()
-    local cur = vim.fn.line "."
-    local target_line = vim.tbl_contains(keybinds, cur) and cur + 2 or keybinds[1]
-    vim.api.nvim_win_set_cursor(0, { target_line, math.floor(vim.o.columns / 2) - 18 })
-  end, { buffer = true })
-
-  vim.keymap.set("n", "<Up>", function()
-    local cur = vim.fn.line "."
-    local target_line = vim.tbl_contains(keybinds, cur) and cur - 2 or keybinds[#keybinds]
-    vim.api.nvim_win_set_cursor(0, { target_line, math.floor(vim.o.columns / 2) - 18 })
-  end, { buffer = true })
-  vim.keymap.set("n", "<Down>", function()
-    local cur = vim.fn.line "."
-    local target_line = vim.tbl_contains(keybinds, cur) and cur + 2 or keybinds[1]
-    vim.api.nvim_win_set_cursor(0, { target_line, math.floor(vim.o.columns / 2) - 18 })
-  end, { buffer = true })
-
-  -- on enter
   vim.keymap.set("n", "<CR>", function()
     for i, val in ipairs(keybinds) do
-      if val == vim.fn.line "." then
-        local action = buttons[i][3]
-
+      if val == vim.fn.line(".") then
+        local action = buttons[i][3] -- 3rd element of the buttons table
         if type(action) == "string" then
           vim.cmd(action)
         elseif type(action) == "function" then
@@ -244,7 +237,6 @@ local function open()
 
   -----------------
 end
-
 -- }}}
 
 -- {{{ Highlights for the DB
@@ -259,19 +251,18 @@ for group, properties in pairs(highlights) do
 end
 -- }}}
 
+-- {{{ Open the dashboard
+vim.defer_fn(function() open() end, 0)
+-- }}}
+
 -- {{{ Auto-resize the Dashboard
--- Function adpoted from: https://github.com/chadcat7/kodo/blob/main/lua/themes/hl.lua
 vim.api.nvim_create_autocmd("VimResized", {
   callback = function()
     if vim.bo.filetype == "TheovimDashboard" then
       vim.opt_local.modifiable = true
       vim.api.nvim_buf_set_lines(0, 0, -1, false, { "" })
-      open()
+      vim.defer_fn(function() open() end, 0)
     end
   end,
 })
--- }}}
-
--- {{{ Open the dashboard
-vim.defer_fn(function() open() end, 0)
 -- }}}
