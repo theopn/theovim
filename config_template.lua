58a4d8567f9d2adad0b1635a39dcee324e7a4114
Theo P <theo.park.n@gmail.com>
2023-07-16 15:06:15 +0900

    fix(config): fix config template directory

--- config_template.lua
+++
@@ -1,6 +0,0 @@
--- Include your *Lua* personal configuration below
--- Find the example configuration at ~/.theovim/config_template.lua
-
--- Setting PDF viewer for a live compilation. Skim for Mac and Zathura for Linux
-vim.g.vimtex_view_method = "skim"
---vim.g.vimtex_view_method = "zathura"
