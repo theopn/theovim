267f05ae73b45652e5bd62ca4809235c5884eca4
Theo P <theo.park.n@gmail.com>
2023-10-03 21:00:42 +0900

    perf(ts): defer ts setup to improve startup time

--- lua/ts.lua
+++ lua/ts.lua
@@ -6,19 +6,22 @@
 -- Configuration for the Neovim's built-in treesitter highlight
 --]]
 
-require("nvim-treesitter.configs").setup {
-  ensure_installed = { "bash", "c", "cpp", "latex", "lua", "markdown", "python", },
-  auto_install = false,
+-- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
+vim.defer_fn(function()
+  require("nvim-treesitter.configs").setup {
+    ensure_installed = { "bash", "c", "cpp", "latex", "lua", "markdown", "python", },
+    auto_install = false,
 
-  highlight = { enable = true, },
-  indent = { enable = true },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<C-space>",
-      node_incremental = "<C-space>",
-      scope_incremental = "<C-s>",
-      node_decremental = "<M-space>",
+    highlight = { enable = true, },
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = "<C-s>",
+        node_decremental = "<M-space>",
+      },
     },
-  },
-}
+  }
+end, 0)
