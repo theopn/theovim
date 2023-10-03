183e700628271f3127bb452b16b90530f302acff
Theo P <theo.park.n@gmail.com>
2023-10-03 12:16:36 +0900

    feat(ts): add incremental selection

--- lua/ts.lua
+++ lua/ts.lua
@@ -7,18 +7,18 @@
 --]]
 
 require("nvim-treesitter.configs").setup {
-  ensure_installed = { "bash", "c", "lua", "markdown", "python", "vim" },
+  ensure_installed = { "bash", "c", "cpp", "latex", "lua", "markdown", "python", },
   auto_install = false,
 
-  highlight = {
-    enable = true,
-    disable = function(lang, buf)
-      local max_filesize = 100 * 1024 -- 100 KB
-      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-      if ok and stats and stats.size > max_filesize then
-        return true
-      end
-    end,
-  },
+  highlight = { enable = true, },
   indent = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<C-space>",
+      node_incremental = "<C-space>",
+      scope_incremental = "<C-s>",
+      node_decremental = "<M-space>",
+    },
+  },
 }
