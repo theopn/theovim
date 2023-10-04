f4de6cbddf418b9a110dd741ca04d3e2c648801c
Theo P <theo.park.n@gmail.com>
2023-10-04 22:00:32 +0900

    refactor(tele): change telescope key chain from <leader>s to <leader>f

--- lua/tele.lua
+++ lua/tele.lua
@@ -24,8 +24,9 @@ telescope.setup({
   extensions = { file_browser = { hidden = true } },
 })
 
--- Load file browser extension AFTER telescope is initialized
+-- Load extension after telescope is initialized
 telescope.load_extension("file_browser")
+pcall(require("telescope").load_extension, "fzf")
 
 -- Git keymaps
 vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Search [G]it [F]iles" })
@@ -34,17 +35,16 @@ vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "[G]it [S]tatus" 
 -- One letter, frequently used keymaps
 vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
 vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" })
--- [S]earch mnemonics keymaps
-vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch for [S]earch: Telescope functions" })
-vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-vim.keymap.set("n", "<leader>sr", builtin.registers, { desc = "[S]earch [R]egisters" })
-vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-vim.keymap.set("n", "<leader>s/", function()
+-- [F]ind mnemonics keymaps
+vim.keymap.set("n", "<leader>ft", builtin.builtin, { desc = "[F]ind [T]elescope: Telescope functions" })
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by rip[G]rep" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
+vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
+vim.keymap.set("n", "<leader>f/", function()
   builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
     previewer = false,
   }))
-end, { desc = "[S]earch [/] Open fuzzy finder for the current buffer words" })
+end, { desc = "[F]ind [/] Open fuzzy finder for the current buffer words" })
 
-vim.keymap.set("n", "<leader>sb", telescope.extensions.file_browser.file_browser, { desc = "[S]earch [B]rowser" })
+vim.keymap.set("n", "<leader>fb", telescope.extensions.file_browser.file_browser, { desc = "[F]ile [B]rowser" })
