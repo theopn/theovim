318ae719500faaa38af1342d0dce6d9d6611f248
Theo P <theo.park.n@gmail.com>
2023-10-06 18:14:11 +0900

    feat(plugins): add keychains to whihc-key config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -111,7 +111,17 @@ local plugins = {
   -- Keybinding guide
   {
     "folke/which-key.nvim",
-    opts = {},
+    config = function()
+      require("which-key").setup()
+      require("which-key").register({
+        ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+        ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+        ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
+        ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+        ["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
+        ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+      })
+    end
   },
 
   -- Color highlighter
