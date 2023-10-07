d955beed1f4a758ebb70832f3befd541a6d90543
Theo P <theo.park.n@gmail.com>
2023-10-07 23:04:18 +0900

    feat(lsp): add keybinding for formatting

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -87,6 +87,8 @@ local on_attach = function(_, bufnr)
     vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
   end
 
+  nmap("<leader>F", vim.lsp.buf.format, "[F]ormat")
+
   -- Frequently used keybindings
   nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
   nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
