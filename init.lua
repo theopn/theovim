1c0ec7b78f5120c40ed74ad16ac92b7b84719230
Theo P <theo.park.n@gmail.com>
2023-10-07 23:01:04 +0900

    refactor(init): move neovide config to init

--- init.lua
+++ init.lua
@@ -19,7 +19,13 @@
 -- Initialize all configuration files
 --]]
 
--- Try catch for modules
+--[[ safe_require()
+-- Try calling `require` for the given module.
+-- If unsuccessful, print the error message using `vim.notify()`
+--
+-- @arg module: Module to `require`
+-- @returns Resultant module from `pcall` if the call was successful, otherwise nil
+--]]
 local function safe_require(module)
   local status, loaded_module = pcall(require, module)
   if status then
@@ -28,9 +34,6 @@ local function safe_require(module)
   vim.notify("Error loading the module: " .. module)
   return nil
 end
-
--- User configuration
-safe_require("config")
 
 -- Core config modules
 safe_require("core")
@@ -45,3 +48,19 @@ safe_require("lsp")
 
 -- Theovim built-in UI elements
 safe_require("ui")
+
+-- Theo's Neovide settings
+if vim.g.neovide then
+  local padding = 10
+  vim.g.neovide_padding_top = padding
+  vim.g.neovide_padding_bottom = padding
+  vim.g.neovide_padding_right = padding
+  vim.g.neovide_padding_left = padding
+
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+
+  vim.g.neovide_transparency = 0.69
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+end
