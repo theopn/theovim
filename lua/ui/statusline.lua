ca16f5b53c184c3d0244e5c11f2fe39354b1dc8a
Theo P <theo.park.n@gmail.com>
2023-10-08 22:52:00 +0900

    feat(ui): finalize the look of new statusline

--- lua/ui/statusline.lua
+++ lua/ui/statusline.lua
@@ -13,14 +13,15 @@
 ---
 ---@alias __statusline_section string Section string.
 
-local components = require("ui.components")
-
 Statusline = {} --> global so that luaeval can keep calling build() function
 
+--- [Credit] Direct port of Mini.Statusline module
 --- Helper determining whether the buftype is normal
---- For more information see ":h buftype"
 ---@return boolean is_normal_buf
-Statusline.isnt_normal_buffer = function() return vim.bo.buftype ~= "" end
+Statusline.isnt_normal_buffer = function()
+  -- For more information see ":h buftype"
+  return vim.bo.buftype ~= ""
+end
 
 -- Metatable containing shorthand notation of the mode and hilight group to be used
 local CTRL_S = vim.api.nvim_replace_termcodes("<C-S>", true, true, true)
@@ -48,8 +49,7 @@ Statusline.modes = setmetatable({
   })
 
 
---- Get filetype icon using devicons
----@return ... __statusline_section
+--- [Credit] Direct port of Mini.Statusline module
 Statusline.get_filetype_icon = function()
   -- Have this `require()` here to not depend on plugin initialization order
   local has_devicons, devicons = pcall(require, 'nvim-web-devicons')
@@ -60,6 +60,7 @@ Statusline.get_filetype_icon = function()
 end
 
 
+--- [Credit] Direct port of Mini.Statusline module
 --- Section for Git information
 ---
 --- Normal output contains name of `HEAD` (via |b:gitsigns_head|) and chunk
@@ -87,6 +88,7 @@ Statusline.section_git = function()
 end
 
 
+--- [Credit] Direct port of Mini.Statusline module
 --- Section for Neovim's builtin diagnostics
 ---
 --- Shows nothing if there is no attached LSP clients or for short output.
@@ -95,47 +97,44 @@ end
 ---
 ---@return __statusline_section
 Statusline.diagnostics = function()
-  if #(vim.lsp.get_active_clients()) == 0 then
-    return ""
-  end
-
+  -- Added by me
   local diagnostic_levels = {
-    { id = vim.diagnostic.severity.ERROR, sign = "E" },
-    { id = vim.diagnostic.severity.WARN,  sign = "W" },
-    { id = vim.diagnostic.severity.INFO,  sign = "I" },
-    { id = vim.diagnostic.severity.HINT,  sign = "H" },
+    { id = vim.diagnostic.severity.ERROR, sign = 'E' },
+    { id = vim.diagnostic.severity.WARN,  sign = 'W' },
+    { id = vim.diagnostic.severity.INFO,  sign = 'I' },
+    { id = vim.diagnostic.severity.HINT,  sign = 'H' },
   }
+  local get_diagnostic_count = function(id) return #vim.diagnostic.get(0, { severity = id }) end
+  -- Added by me
+
+  -- Assumption: there are no attached clients if table
+  -- `vim.lsp.buf_get_clients()` is empty
+  local hasnt_attached_client = next(vim.lsp.buf_get_clients()) == nil
+  --local dont_show_lsp = MiniStatusline.is_truncated(args.trunc_width) or H.isnt_normal_buffer() or hasnt_attached_client
+  local dont_show_lsp = Statusline.isnt_normal_buffer() or hasnt_attached_client
+  if dont_show_lsp then return '' end
+
+  -- Construct diagnostic info using predefined order
   local t = {}
+  --for _, level in ipairs(H.diagnostic_levels) do
   for _, level in ipairs(diagnostic_levels) do
-    local n = #vim.diagnostic.get(0, { severity = level.id })
-    if n > 0 then table.insert(t, string.format(" %s%s", level.sign, n)) end
+    --local n = H.get_diagnostic_count(level.id)
+    local n = get_diagnostic_count(level.id)
+    -- Add level info only if diagnostic is present
+    if n > 0 then table.insert(t, string.format(' %s%s', level.sign, n)) end
   end
 
-  local icon = ""
-  if vim.tbl_count(t) == 0 then return ("%s -"):format(icon) end
-  return string.format("%s%s", icon, table.concat(t, ""))
+  --local icon = args.icon or (H.get_config().use_icons and '' or 'LSP')
+  local icon = ''
+  if vim.tbl_count(t) == 0 then return ('%s -'):format(icon) end
+  return string.format('%s%s', icon, table.concat(t, ''))
 end
 
 
----
----
----@return __statusline_section
-Statusline.filzesize = function()
-  local size = vim.fn.getfsize(vim.fn.getreg("%"))
-  if size < 1024 then
-    return string.format('%dB', size)
-  elseif size < 1048576 then
-    return string.format('%.2fKiB', size / 1024)
-  else
-    return string.format('%.2fMiB', size / 1048576)
-  end
-end
-
-
----
+--- Format Filetype (%Y through Statusline field), file size, fileformat, and encoding
 ---
 ---@return __statusline_section
-Statusline.ff_and_enc = function()
+Statusline.file_info = function()
   local ff = vim.bo.fileformat
   if ff == "unix" then
     ff = ""
@@ -144,7 +143,18 @@ Statusline.ff_and_enc = function()
   end
   -- If new file does not have encoding, display global encoding
   local enc = (vim.bo.fileencoding == "") and (vim.o.encoding) or (vim.bo.fileencoding)
-  return string.format("%s :%s", ff, enc):upper()
+
+  -- Calculate size
+  local size = vim.fn.getfsize(vim.fn.getreg("%"))
+  if size < 1024 then
+    size = string.format("%dB", size)
+  elseif size < 1048576 then
+    size = string.format("%.2fKiB", size / 1024)
+  else
+    size = string.format("%.2fMiB", size / 1048576)
+  end
+
+  return string.format(" %%Y | %s | %s | %s ", size, ff, enc:upper())
 end
 
 
@@ -154,12 +164,13 @@ end
 --- [Mode] filename_tail[mod][RO] diagit        line : column
 ---
 --- Else, output the following Statusline:
---- [Mode] CWD filepath[mod][RO] diagit        file_size filetype file_format:enc line/total_line : col/total_col
+--- [Mode] CWD filepath[mod][RO] diagit        filetype file_size file_format:enc line/total_line : col/total_col
 ---
 ---@return string Statusline appropriately sized statusline
 Statusline.active = function()
   local cur_width = vim.o.laststatus == 3 and vim.o.columns or vim.api.nvim_win_get_width(0)
   local is_short = (cur_width < 100)
+
   local mode_info = Statusline.modes[vim.fn.mode()]
 
   -- Filepath/filename
@@ -169,14 +180,18 @@ Statusline.active = function()
       or string.format("  %s %s %%f%%m%%r ", vim.fn.fnamemodify(vim.fn.getcwd(), ":t"), Statusline.get_filetype_icon())
 
   -- Combine Diagnostics and Git info
-  local diagnostics_info = Statusline.diagnostics()
+  local diagit = Statusline.diagnostics()
   local git_info = Statusline.section_git()
-  local diagit = ""
-  if diagnostics_info ~= "" and git_info ~= "" then
-    diagit = string.format("%s | %s", diagnostics_info, git_info)
-  elseif diagnostics_info == "" then
+  if diagit ~= "" and git_info ~= "" then
+    diagit = string.format("%s | %s", diagit, git_info)
+  elseif diagit == "" then
     diagit = git_info
   end
+
+  -- Fileinfo
+  -- If short Statusline: ""
+  -- Else: Filetype | Filesize | File format (Unix or Dos) | Encoding
+  local file_info = is_short and "" or Statusline.file_info()
 
   -- Location
   -- If short Statusline: line : col
@@ -184,44 +199,20 @@ Statusline.active = function()
   local location = is_short and string.format("%s %s:%s ", mode_info.hl, "%l", "%2v")
       or string.format("%s %s/%s:%s/%s ", mode_info.hl, "%l", "%L", "%2v", '%-2{virtcol("$") - 1}')
 
-  if cur_width < 100 then
-    -- Short statusline
-    return table.concat({
-      string.format("%s %s ", mode_info.hl, mode_info.name),
-      "%#MiniStatuslineFilename#",
-      path,
-      -- Make above modules the last to be truncated
-      "%<",
-
-      -- LSP and Git
-      "%#MiniStatuslineFileinfo# ",
-      diagit,
-
-      -- Spacer
-      "%=",
-
-      location,
-    })
-  else
-    -- Long Statusline
-    return table.concat({
-      string.format("%s %s ", mode_info.hl, mode_info.name),
-      "%#MiniStatuslineFilename#",
-      path,
-      -- Make above modules the last to be truncated
-      "%<",
-
-      -- LSP and Git
-      "%#MiniStatuslineDevinfo# ",
-      diagit,
-      -- Spacer
-      "%=",
-      -- Extra file information
-      "%#MiniStatuslineFilename#",
-      string.format(" %s | %s | %s ", Statusline.filzesize(), "%Y", Statusline.ff_and_enc()),
-      location,
-    })
-  end
+  return table.concat({
+    string.format("%s %s ", mode_info.hl, mode_info.name),
+    "%#MiniStatuslineFilename#",
+    path,
+    -- Make above modules the last to be truncated
+    "%<",
+    "%#MiniStatuslineDevinfo# ",
+    diagit,
+    -- Spacer
+    "%=",
+    "%#MiniStatuslineFilename#",
+    file_info,
+    location,
+  })
 end
 
 --- Build a simple Statusline for inactive windows
