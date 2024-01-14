bd40304824c2402c3360a0f69220f34916c99318
Theo P <theo.park.n@gmail.com>
2024-01-14 15:36:00 +0900

    refactor: change [f]ind mnemonics to [s]earch

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -115,7 +115,7 @@ local plugins = {
         ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
         ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
         ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-        ["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
+        ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
         ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
       })
     end
