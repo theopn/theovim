bed4890259fb9b974c730b1dd3763ed5025c3d39
Theo P <theo.park.n@gmail.com>
2024-03-29 22:34:15 +0900

    style(oil): make oil.lua directly return the table since it is a short config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,9 @@
-local M = { "stevearc/oil.nvim" }
-
-M.opts = {
-  default_file_explorer = false, --> do not hijack netrw
+return {
+  "stevearc/oil.nvim",
+  dependencies = {
+    { "nvim-tree/nvim-web-devicons" },
+  },
+  opts = {
+    default_file_explorer = false, --> do not hijack netrw
+  },
 }
-
-M.dependencies = {
-  { "nvim-tree/nvim-web-devicons" },
-}
-
-return M
