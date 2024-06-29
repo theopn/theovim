8cdbe866b79b7c20a5436dc341be5b3651a58dac
Theo P <theo.park.n@gmail.com>
2024-06-29 15:05:15 +0900

    feat(lsp): add inlay hints toggle and optimize lsp highlight mechanism

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -28,7 +28,7 @@ M.config = function()
     group = theovim_lsp_config_group,
     callback = function(event)
       --- Inline helper for defining a buffer-scope LSP keymap with a description
-      local nmap = function(keys, func, desc)
+      local map = function(keys, func, desc)
         vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
       end
 
@@ -37,49 +37,66 @@ M.config = function()
       if status then
         local telescope_opt = { jump_type = "tab" } --> Spawn selection in a new tab
         -- Navigation
-        nmap("gd", function() builtin.lsp_definitions(telescope_opt) end, "[G]oto [D]efinition")
-        nmap("gr", builtin.lsp_references, "[G]oto [R]eferences")
-        nmap("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
-        nmap("<leader>D", function() builtin.lsp_type_definitions(telescope_opt) end, "Type [D]efinition")
+        map("gd", function() builtin.lsp_definitions(telescope_opt) end, "[G]oto [D]efinition")
+        map("gr", builtin.lsp_references, "[G]oto [R]eferences")
+        map("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
+        map("<leader>D", function() builtin.lsp_type_definitions(telescope_opt) end, "Type [D]efinition")
         -- Symbols
-        nmap("<leader>ds", builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
-        nmap("<leader>ws", builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+        map("<leader>ds", builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
+        map("<leader>ws", builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
       else
-        nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-        nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-        nmap("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
-        nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-        nmap("<leader>ds", vim.lsp.buf.document_symbol, "[D]ocument [S]ymbols")
-        nmap("<leader>ws", vim.lsp.buf.workspace_symbol, "[W]orkspace [S]ymbols")
+        map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+        map("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+        map("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
+        map("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+        map("<leader>ds", vim.lsp.buf.document_symbol, "[D]ocument [S]ymbols")
+        map("<leader>ws", vim.lsp.buf.workspace_symbol, "[W]orkspace [S]ymbols")
       end
 
       -- No Telescope counterparts
-      nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-      nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-      nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-      nmap("gD", vim.lsp.buf.declaration, "[G]o [D]eclaration")
+      map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+      map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+      map("K", vim.lsp.buf.hover, "Hover Documentation")
+      map("gD", vim.lsp.buf.declaration, "[G]o [D]eclaration")
 
       -- Highlight keyword under the cursor
       local client = vim.lsp.get_client_by_id(event.data.client_id)
       if client and client.server_capabilities.documentHighlightProvider then
+        local theovim_lsp_hl_group = vim.api.nvim_create_augroup("TheovimLspHl", { clear = false, })
         vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-          group = theovim_lsp_config_group,
+          group = theovim_lsp_hl_group,
           buffer = event.buf,
           callback = vim.lsp.buf.document_highlight,
         })
 
         vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-          group = theovim_lsp_config_group,
+          group = theovim_lsp_hl_group,
           buffer = event.buf,
           callback = vim.lsp.buf.clear_references,
         })
+
+        local theovim_lsp_detach_group = vim.api.nvim_create_augroup("TheovimLspHlDetach", { clear = true, })
+        vim.api.nvim_create_autocmd("LspDetach", {
+          group = theovim_lsp_detach_group,
+          callback = function(event2)
+            vim.lsp.buf.clear_references()
+            vim.api.nvim_clear_autocmds({ group = "TheovimLspHl", buffer = event2.buf })
+          end
+        })
+      end
+
+      -- Inlay hints
+      if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+        map("<leader>th", function()
+          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+        end, "[T]oggle Inlay [H]ints")
       end
 
       -- Format command
       vim.api.nvim_buf_create_user_command(event.buf, "Format", function(_)
         vim.lsp.buf.format()
       end, { desc = "Format current buffer with LSP" })
-      nmap("<leader>F", vim.lsp.buf.format, "[F]ormat")
+      map("<leader>F", vim.lsp.buf.format, "[F]ormat")
     end,
   })
 
@@ -111,7 +128,7 @@ M.config = function()
   -- 1. ensure servers are installed
   -- 2. Set up handlers for each server using the `server` table
   require("mason-lspconfig").setup({
-    ensure_installed = vim.tbl_keys(servers),
+    ensure_installed = vim.tbl_keys(servers or {}),
     -- Separate setup_handlers() function could be used for the same purpose.
     -- Read |mason-lspconfig.setup_handlers()| for more information
     handlers = {
