66b4eaa21ef40e88fe5991775545cdb3673113e1
Theo P <theo.park.n@gmail.com>
2024-07-13 16:01:37 +0900

    fix: fix which-key deprecation error by using add instead of register func

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,15 +3,25 @@ return {
   event = "VimEnter",
   config = function()
     require("which-key").setup()
-    require("which-key").register({
-      ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-      ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-      ["<leader>f"] = { name = "[F]ile", _ = "which_key_ignore" },
-      ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-      ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-      ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-      ["<leader>t"] = { name = "[T]oggle / [T]erminal", _ = "which_key_ignore" },
-      ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-    })
+    require("which-key").add(
+      {
+        { "<leader>c",  group = "[C]ode" },
+        { "<leader>c_", hidden = true },
+        { "<leader>d",  group = "[D]ocument" },
+        { "<leader>d_", hidden = true },
+        { "<leader>f",  group = "[F]ile" },
+        { "<leader>f_", hidden = true },
+        { "<leader>g",  group = "[G]it" },
+        { "<leader>g_", hidden = true },
+        { "<leader>r",  group = "[R]ename" },
+        { "<leader>r_", hidden = true },
+        { "<leader>s",  group = "[S]earch" },
+        { "<leader>s_", hidden = true },
+        { "<leader>t",  group = "[T]oggle / [T]erminal" },
+        { "<leader>t_", hidden = true },
+        { "<leader>w",  group = "[W]orkspace" },
+        { "<leader>w_", hidden = true },
+      }
+    )
   end
 }
