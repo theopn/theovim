ea5046e51026c096cfcd2e80686db20b927a7570
Theo P <theo.park.n@gmail.com>
2024-07-27 13:09:52 +0900

    feat(lazy): add bootstrap error message

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -8,17 +8,20 @@
 ---
 --- Bootstraps lazy.nvim plugin manager and loads modules in lua/plugins
 
--- Install Lazy.nvim
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      --{ "\nPress any key to exit..." },
+    }, true, {})
+    --vim.fn.getchar()
+    --os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
