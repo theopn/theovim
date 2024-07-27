103b6e3b0c484a6d00b717f43eda5af63edac9ad
Theo P <theo.park.n@gmail.com>
2024-07-27 12:32:46 +0900

    fix(which-key): remove unnecessary ignore key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,25 +3,15 @@ return {
   event = "VimEnter",
   config = function()
     require("which-key").setup()
-    require("which-key").add(
-      {
+    require("which-key").add({
         { "<leader>c",  group = "[C]ode" },
-        { "<leader>c_", hidden = true },
         { "<leader>d",  group = "[D]ocument" },
-        { "<leader>d_", hidden = true },
         { "<leader>f",  group = "[F]ile" },
-        { "<leader>f_", hidden = true },
         { "<leader>g",  group = "[G]it" },
-        { "<leader>g_", hidden = true },
         { "<leader>r",  group = "[R]ename" },
-        { "<leader>r_", hidden = true },
         { "<leader>s",  group = "[S]earch" },
-        { "<leader>s_", hidden = true },
-        { "<leader>t",  group = "[T]oggle / [T]erminal" },
-        { "<leader>t_", hidden = true },
         { "<leader>w",  group = "[W]orkspace" },
-        { "<leader>w_", hidden = true },
-      }
-    )
+        { "<leader>t",  group = "[T]oggle / [T]erminal" },
+      })
   end
 }
